108ef0097cc981dab71f59fbe0204f5d373d5904
Mari Maaloy Alsaker <mmalsaker@gmail.com>
2024-02-13 18:02:22 +0100

    now watching my own sodium images

---
+++ MRImages.py
@@ -0,0 +1,44 @@
+import scipy.io
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+def show_as_video(im_array, vmax:int=1, gif_name:str=None):
+    """ Show an animation of the normalized field magnitude in all slices through z-axis. \n 
+    Parameters:
+    - chosen_field: B or H
+    - vmax: maximum value in coloring (very strong field around loop ~200)
+    - gif_name: if you want to save a movie of the animation give a string: \"name.mp4\""""
+
+    initialized = False
+    imgs = []
+    fig = plt.figure(figsize=(9,7))
+    ax = fig.add_subplot(1,1,1)
+    ticks = np.linspace(0, 100, 5, endpoint=True)
+    abs_array = abs(im_array)
+    normalized_field_magn = abs_array/np.max(abs_array)
+    for f_slc in normalized_field_magn:
+        if not initialized:
+            img = ax.pcolormesh(f_slc, cmap="plasma", vmin=0, vmax=vmax)
+            plt.colorbar(img, label="[%]")
+            ax.set_title(f"Magnitude of complex value")
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            initialized = True
+        else:
+            img = ax.pcolormesh(f_slc, cmap="plasma", vmin=0, vmax=vmax, animated=True)
+        imgs.append([img])
+    ani = animation.ArtistAnimation(fig, imgs, interval=50, blit=True,
+                            repeat_delay=1000)
+    plt.show()
+    if gif_name:
+        writergif = animation.FFMpegWriter(fps=30)
+        ani.save("Figures/"+gif_name, writer=writergif)
+
+
+path = "/Users/marialsaker/MRImages/Single_loop/Exam13509/Series4/"
+
+mat = scipy.io.loadmat(path + "ScanArchive_NO1011MR01_20240209_082731323.mat")
+print(mat.keys())
+vals = mat['bb']
+show_as_video(vals)# gif_name="first_MRI.mp4")
