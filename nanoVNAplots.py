6dc9e30a0e34fb376ff089cf4882a369b533fead
Mari Maaloy Alsaker <mmalsaker@gmail.com>
2024-03-18 11:13:45 +0100

    prepped for new meas

--- nanoVNAplots.py
+++ nanoVNAplots.py
@@ -2,13 +2,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import ticker
 import collections.abc
-#hyper needs the four following aliases to be done manually.
 collections.Iterable = collections.abc.Iterable
 collections.Mapping = collections.abc.Mapping
 collections.MutableSet = collections.abc.MutableSet
 collections.MutableMapping = collections.abc.MutableMapping
-#Now import hyper
-#import hyper
 from smithplot import SmithAxes
 import matplotlib as mpl
 
@@ -50,7 +47,7 @@ def extract_vals_s2p(file_path, len_file=301):
             vals[i][0] = vals[i][0]*1E-6
     return vals # f, r_s11, i_s11, r_s21, i_s21, r_s12, i_s12, r_s22, i_s22
 
-def plot_magn_phase(ax, freq_lists, magn_db_lists, phase_list, magn_colors, phase_colors, show_phase=True, names="S11"):
+def plot_magn_phase(ax, freq_lists, magn_db_lists, phase_list, magn_colors, phase_colors, show_phase=True, names="S11"): # Currently not in use
     for i, freqs in enumerate(freq_lists):
         plots = ax.plot(freqs, magn_db_lists[i], label=f"|S11| {names[i]}", marker=".")
         plots[0].set(color = magn_colors[i])
@@ -84,9 +81,8 @@ def plot_Sparams(ax, freqs, s_magns_db, colors, names):
 def plot_res(ax, magn_db, freqs, color):
     min_index = np.argmin(magn_db)
     min_db = magn_db[min_index]
-    #print(freqs[min_index], color)
-    plots = ax.plot(freqs[min_index], min_db, label=f"min= {min_db:.0f}dB, {freqs[min_index]:.3f}MHz", marker=".")
-    plots[0].set(color=color, marker = "o")
+    plots = ax.plot(freqs[min_index], min_db, label=f"min=({freqs[min_index]:.3f}MHz, {min_db:.0f}dB)", marker=".", zorder=3)
+    plots[0].set(color=color, marker = "o", markersize=8)
     return min_index
 
 def q_factor(freqs, magn_db, reals, ims, z0):
@@ -103,14 +99,14 @@ def q_factor(freqs, magn_db, reals, ims, z0):
     dX = (n*(np.sum(pos_slopews*Xs)) - np.sum(pos_slopews)*np.sum(Xs)) / (n*np.sum(pos_slopews**2) - np.sum(pos_slopews)**2)
     return freqs[index_w0]/(2*R+2*z0) * dX
 
-def plot_smith(ax, reals, ims, c, magn_db, c_spes):
+def plot_smith(ax, reals, ims, c, magn_db):
     """ ax must be projection=smith """
     vals_s11 = (reals + ims * 1j)
     min_index = np.argmin(magn_db)
-    plots = ax.plot(vals_s11, markevery=1, label="equipoints=11", equipoints=11, datatype=SmithAxes.S_PARAMETER)
+    plots = ax.plot(vals_s11, markevery=1, datatype=SmithAxes.S_PARAMETER)
     plots[0].set(color=c)
-    plots = ax.plot(vals_s11[min_index], markevery=1, label="equipoints=11", equipoints=11, datatype=SmithAxes.S_PARAMETER)
-    plots[0].set(color=c_spes, marker="o")
+    plots = ax.plot(vals_s11[min_index], markevery=1, datatype=SmithAxes.S_PARAMETER, markersize=20)
+    plots[0].set(color=c, marker="o")
 
 def find_mean_s11(num_files, file_path, filename, ending=".s1p", file_len = 301):
     for i in range(num_files):
@@ -154,63 +150,75 @@ def find_mean_allS(num_files, file_path, filename, ending=".s2p", file_len = 301
     return mean_data, np.mean(magn_desibels, axis=1), np.std(magn_desibels, axis=1), np.mean(phase, axis=1), np.std(phase, axis=1)
 
 
-""" SINGLE LOOP COIL PLOTS AND MEASUREMENTS """
-show_single_plots = True
-print_Qs_single = False
+# """ SINGLE LOOP COIL PLOTS AND MEASUREMENTS """
+# print_Qs_single = False
 
-mean_data1, std_data1 = find_mean_s11(num_files=10, file_path = folder_pocketvna+"Single_loop/", filename="idun4na", ending=".s2p")
-freqs1, reals1, ims1, magn1_db, phase1 = mean_data1[0], mean_data1[1], mean_data1[2], mean_data1[3], mean_data1[4]
-mean_data2, std_data2 = find_mean_s11(num_files=10, file_path = folder_nanovna+"single_loop/2MHz/", filename="ketchup4na", ending=".s1p")
-freqs2, reals2, ims2, magn2_db, phase2 = mean_data2[0], mean_data2[1], mean_data2[2], mean_data2[3], mean_data2[4]
+# coils = [
+#     {"name" : "Single loop",
+#      "file_loc": f"{folder_pocketvna}Single_loop/"},
+#     {"name" : "Assadi's surface",
+#      "file_loc": f"{folder_pocketvna}AssadiSurface/"},
+#     {"name" : "Original surface",
+#      "file_loc": f"{folder_pocketvna}OrigSurface/"},
+#      {"name" : "Enhancing",
+#      "file_loc": f"{folder_pocketvna}Enhancing/"},
+# ]
+# c_load = "steelblue"
+# c_noload = "orange"
+# loadfile = "idun4na"
+# noloadfile = "noload"
 
-fig = plt.figure(figsize=[6, 10])
-ax1 = fig.add_subplot(2,1,1)
-ax1.vlines(x=[33.78], ymin=-42, ymax=1, colors="k", linestyles="--", label="f0=33.78MHz")
-c1 = "steelblue"
-c2 = "orange"
-#plot_magn_phase(ax1, [freqs1, freqs2], [magn1_db, magn2_db], [phase1, phase2], magn_colors=[c1,c2], phase_colors=[c1,c2], show_phase=False, names=["pocketvna", "nanovna"])
-err_line1 = ax1.errorbar(freqs1, magn1_db, yerr=std_data1[3])
-err_line2 = ax1.errorbar(freqs2, magn2_db, yerr=std_data2[3])
-err_line1.set_label("pocketvna")
-err_line2.set_label("nanovna")
-ax1.grid(True)
-ax2 = fig.add_subplot(2,1,2, projection="smith")
-# Plotting smith charts
-min_index1 = plot_res(ax=ax1, magn_db=magn1_db, freqs=freqs1, color="turquoise")
-plot_smith(ax=ax2, reals=reals1, ims=ims1, c=c1, magn_db=magn1_db, c_spes="turquoise")
-min_index2 = plot_res(ax=ax1, magn_db=magn2_db, freqs=freqs2, color="red")
-plot_smith(ax=ax2, reals=reals2, ims=ims2, c=c2, magn_db=magn2_db, c_spes="red")
-    
-ax1.legend()
-fig.suptitle("Laboratory tests of the single loop coil \n loaded and unloaded")
-ax1.set_title("|S11| magnitude")
-ax2.set_title("Real and imaginary impedance (Smith chart)")
-#plt.savefig('s11_smith_singleloop.png', transparent=True)
-print("STD of magnitude pocketvna = ", std_data1[3][min_index1-1:min_index1+2])
-print("STD of magnitude nanovna = ", std_data2[3][min_index2-1:min_index2+2])
+# for coil in coils:
+#     name = coil["name"]
+#     mean_data1, std_data1 = find_mean_s11(num_files=10, file_path=coil["file_loc"], filename=loadfile, ending=".s2p", file_len=1001)
+#     freqs1, reals1, ims1, magn1_db, phase1 = mean_data1[0], mean_data1[1], mean_data1[2], mean_data1[3], mean_data1[4]
+#     mean_data2, std_data2 = find_mean_s11(num_files=10, file_path=coil["file_loc"], filename=noloadfile, ending=".s2p", file_len=1001)
+#     freqs2, reals2, ims2, magn2_db, phase2 = mean_data2[0], mean_data2[1], mean_data2[2], mean_data2[3], mean_data2[4]
 
-if show_single_plots:
-    plt.show()
-else:
-    plt.close()
+#     fig = plt.figure(figsize=[10, 5])
+#     ax1 = fig.add_subplot(1,2,1)
+#     ax1.vlines(x=[33.78], ymin=-42, ymax=1, colors="k", linestyles="--", label="f0=33.78MHz", zorder=0)
+#     err_line1 = ax1.errorbar(freqs1, magn1_db, yerr=std_data1[3], zorder=1)
+#     err_line2 = ax1.errorbar(freqs2, magn2_db, yerr=std_data2[3], zorder=1)
+#     err_line1.set_label("loaded")
+#     err_line2.set_label("unloaded")
+#     ax1.grid(True)
+#     ax2 = fig.add_subplot(1,2,2, projection="smith")
+#     # Plotting smith charts
+#     min_index1 = plot_res(ax=ax1, magn_db=magn1_db, freqs=freqs1, color=c_load)
+#     plot_smith(ax=ax2, reals=reals1, ims=ims1, c=c_load, magn_db=magn1_db)
+#     min_index2 = plot_res(ax=ax1, magn_db=magn2_db, freqs=freqs2, color=c_noload)
+#     plot_smith(ax=ax2, reals=reals2, ims=ims2, c=c_noload, magn_db=magn2_db)
+        
+#     ax1.legend()
+#     fig.suptitle(f"Laboratory tests of {name}")
+#     ax1.set_title("|S11| magnitude")
+#     ax2.set_title("Real and imaginary impedance (Smith chart)")
+#     #plt.savefig('s11_smith_singleloop.png', transparent=True)
+#     print("STD of magnitude loaded = ", std_data1[3][min_index1-1:min_index1+2])
+#     print("STD of magnitude unloaded = ", std_data2[3][min_index2-1:min_index2+2])
 
-Q_load = q_factor(freqs1, magn1_db, reals1, ims1, z0=50)
-Q_noload = q_factor(freqs2, magn2_db, reals2, ims2, z0=50)
-Q_ratio = Q_noload/Q_load
-if print_Qs_single:
-    print("Q factor and ratio for single loop coil:")
-    print(f"Unloaded Q = {Q_noload:.4f} and loaded Q = {Q_load:.4f}\n> Q ratio = {Q_ratio:.4f}")
+#     Q_load = q_factor(freqs1, magn1_db, reals1, ims1, z0=50)
+#     Q_noload = q_factor(freqs2, magn2_db, reals2, ims2, z0=50)
+#     Q_ratio = Q_noload/Q_load
+#     if print_Qs_single:
+#         print("Q factor and ratio for single loop coil:")
+#         print(f"Unloaded Q = {Q_noload:.4f} and loaded Q = {Q_load:.4f}\n> Q ratio = {Q_ratio:.4f}")
+
+# plt.show()
+# plt.close("all")
+
 
 
 """ QUADRATURE COIL PLOTS AND MEASUREMENTS """
 # Demonstrate that the quadrature coil is a reciprocal network, meaning that the S_21 = S_12
-show_quad_plots = False
+show_quad_plots = True
 save = False
 print_impedance = False
 print_Qs_quad = False
 
-values_unloaded_quad = find_mean_allS(num_files=10, file_path=folder_pocketvna+"Quad_loop/",filename="noload", file_len=1001)
-values_loaded_quad = find_mean_allS(num_files=10, file_path=folder_pocketvna+"Quad_loop/",filename="idun4na", file_len=1001)
+values_unloaded_quad = find_mean_allS(num_files=10, file_path=folder_pocketvna+"Quad_loop/First/",filename="noload", file_len=1001)
+values_loaded_quad = find_mean_allS(num_files=10, file_path=folder_pocketvna+"Quad_loop/First/",filename="siemens", file_len=1001)
 # values = [mean_data, std_magn_db, mean_magn_db, mean_phase, std_phase]
 # mean_data = [f, r_s11, i_s11, r_s21, i_s21, r_s12, i_s12, r_s22, i_s22]
 # mean/std = [s11, s22, s21, s12]
@@ -233,8 +241,8 @@ for i, values in enumerate([values_loaded_quad, values_unloaded_quad]):
     else:
         names = ["s_11 no load", "s_21 no load", "s_12 no load", "s_22 no load"]
     plot_Sparams(ax=ax, freqs=freqs_q, s_magns_db=values[1], colors=colors, names=names)
-    min_i_first = plot_res(ax, magn_db=values[1][0], freqs=freqs_q, color="red")
-    min_i_switch = plot_res(ax, magn_db=values[1][3], freqs=freqs_q, color="darkblue")
+    min_i_first = plot_res(ax, magn_db=values[1][0], freqs=freqs_q, color=colors[0])
+    min_i_switch = plot_res(ax, magn_db=values[1][3], freqs=freqs_q, color=colors[3])
     print(f"Minimums indexes = {min_i_first}, {min_i_switch}")
     ax.set_title(f"S parameter magnitudes,\nquadrature coil was loaded with {loadTF[i]}")
     ax.legend()
@@ -255,9 +263,9 @@ for i, values in enumerate([values_loaded_quad, values_unloaded_quad]):
     Qs.append((Q_switch+Q_first)/2)
 
     plot_smith(ax=smithax, reals=s_11s[1], ims=s_11s[2], 
-               c=colors[0], magn_db=(s_11s[3]), c_spes="red")
+               c=colors[0], magn_db=(s_11s[3]))
     plot_smith(ax=smithax, reals=s_22s[1], ims=s_22s[2], 
-               c=colors[-1], magn_db=(s_22s[3]), c_spes="darkblue")
+               c=colors[-1], magn_db=(s_22s[3]))
     diffs = [freqs_q[i+1]-freqs_q[i] for i in range(len(freqs_q)-1)]
     mean_diff = np.mean(diffs)
     print(f"Uncertainties for magnitude with load {loadTF[i]} at resonance: ",
@@ -276,40 +284,40 @@ else:
     plt.close("all")
 
 
-""" HYBRID PLOTS AND MEASUREMENTS """
+# """ HYBRID PLOTS AND MEASUREMENTS """
 
-hybrid_data = ["hybrid_setup1_isoSMA", "hybrid_setup6_isoBNC", "hybrid_setup2_through", "hybrid_setup3_through", 
-               "hybrid_setup4_through", "hybrid_setup5_through"]
-hybrid_setups = ["Hybrid, coil coax","Hybrid, MRI coax", "Hybrid through, green to same", "Hybrid through, green to opposite", 
-               "Hybrid through, blue to same", "Hybrid through, blue to opposite"]
-width, height = 9, 5
-rows, cols = 1, 2
-axs = []
-for i in range(3):
-    fig = plt.figure(figsize=[width, height])
-    ax = fig.subplots(rows, cols)
-    axs.append(ax)
-colors = [cmap(0.2), cmap(0.4), cmap(0.6), cmap(0.8)]
-labels = ["s_11", "s_21", "s_12", "s_22"]
-axs = [axs[0][0], axs[0][1], axs[1][0], axs[1][1], axs[2][0], axs[2][1]]
-prev = 0
-for i, name in enumerate(hybrid_data):
-    vals = find_mean_allS(num_files=10, file_path=folder_pocketvna+"Quad_loop/",filename=name, file_len=501)
-    freqs_h = vals[0][0]
-    res = next(x for x, val in enumerate(freqs_h) if val > 33.775)
-    plot_Sparams(ax=axs[i], freqs=freqs_h, s_magns_db=vals[1], colors=colors, names=labels)
-    axs[i].vlines(x=[33.78],ymin=-55, ymax=1, colors=["k"], linestyles="--", label="f=33.78MHz")
-    axs[i].set_title(hybrid_setups[i])
-    axs[i].legend()
-    print("\n", hybrid_setups[i])
-    s_sequence = ["S_11", "S_22", "S_21", "S_12"]
-    # mean_data, std_magn_db, mean_magn_db, mean_phase, std_phase
-    for i in range(4):
-        print(f"Mag of {s_sequence[i]} = {vals[1][i][res]:.5f} +/- {vals[2][i][res]:.5f}")
-        print(f"Phase of {s_sequence[i]} = {vals[3][i][res]:.5f} +/- {vals[4][i][res]:.5f}")
+# hybrid_data = ["hybrid_setup1_isoSMA", "hybrid_setup6_isoBNC", "hybrid_setup2_through", "hybrid_setup3_through", 
+#                "hybrid_setup4_through", "hybrid_setup5_through"]
+# hybrid_setups = ["Hybrid, coil coax","Hybrid, MRI coax", "Hybrid through, green to same", "Hybrid through, green to opposite", 
+#                "Hybrid through, blue to same", "Hybrid through, blue to opposite"]
+# width, height = 9, 5
+# rows, cols = 1, 2
+# axs = []
+# for i in range(3):
+#     fig = plt.figure(figsize=[width, height])
+#     ax = fig.subplots(rows, cols)
+#     axs.append(ax)
+# colors = [cmap(0.2), cmap(0.4), cmap(0.6), cmap(0.8)]
+# labels = ["s_11", "s_21", "s_12", "s_22"]
+# axs = [axs[0][0], axs[0][1], axs[1][0], axs[1][1], axs[2][0], axs[2][1]]
+# prev = 0
+# for i, name in enumerate(hybrid_data):
+#     vals = find_mean_allS(num_files=10, file_path=folder_pocketvna+"Quad_loop/Hybrid/",filename=name, file_len=501)
+#     freqs_h = vals[0][0]
+#     res = next(x for x, val in enumerate(freqs_h) if val > 33.775)
+#     plot_Sparams(ax=axs[i], freqs=freqs_h, s_magns_db=vals[1], colors=colors, names=labels)
+#     axs[i].vlines(x=[33.78],ymin=-55, ymax=1, colors=["k"], linestyles="--", label="f=33.78MHz")
+#     axs[i].set_title(hybrid_setups[i])
+#     axs[i].legend()
+#     print("\n", hybrid_setups[i])
+#     s_sequence = ["S_11", "S_22", "S_21", "S_12"]
+#     # mean_data, std_magn_db, mean_magn_db, mean_phase, std_phase
+#     for i in range(4):
+#         print(f"Mag of {s_sequence[i]} = {vals[1][i][res]:.5f} +/- {vals[2][i][res]:.5f}")
+#         print(f"Phase of {s_sequence[i]} = {vals[3][i][res]:.5f} +/- {vals[4][i][res]:.5f}")
 
-print(f"\nPhase difference green s21 is {0.103-(-1.383):.3f} +/- {np.sqrt(0.004**2+0.002**2):.3f}")
-print(f"Phase difference green s12 is {0.098-(-1.378):.3f} +/- {np.sqrt(0.003**2+0.003**2):.3f}")
-print(f"Phase difference blue s21 is {0.116-(-1.370):.3f} +/- {np.sqrt(0.006**2+0.002**2):.3f}")
-print(f"Phase difference blue s12 is {0.112-(-1.368):.3f} +/- {np.sqrt(0.006**2+0.002**2):.3f}")
-plt.show()
+# print(f"\nPhase difference green s21 is {0.103-(-1.383):.3f} +/- {np.sqrt(0.004**2+0.002**2):.3f}")
+# print(f"Phase difference green s12 is {0.098-(-1.378):.3f} +/- {np.sqrt(0.003**2+0.003**2):.3f}")
+# print(f"Phase difference blue s21 is {0.116-(-1.370):.3f} +/- {np.sqrt(0.006**2+0.002**2):.3f}")
+# print(f"Phase difference blue s12 is {0.112-(-1.368):.3f} +/- {np.sqrt(0.006**2+0.002**2):.3f}")
+# plt.show()
